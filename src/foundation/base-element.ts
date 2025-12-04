895417cd039b9862b874ebcad1e7aef276826585
keinerapo <keiner.apajaro@gmail.com>
2025-12-04 10:31:09 +0100

    feat: add Lit and create BaseElement foundation

---
+++ src/foundation/base-element.ts
@@ -0,0 +1,14 @@
+import { LitElement, css } from 'lit';
+
+export abstract class BaseElement extends LitElement {
+  static styles = css`
+    :host {
+      box-sizing: border-box;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+    }
+      
+    :host([hidden]) {
+      display: none !important;
+    }
+  `;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './foundation/base-element.js';
